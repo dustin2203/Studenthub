d655e49644b5d4cd2dab0257910ff27a8d397f91
Dustin2203 <dustin2203@googlemail.com>
2019-11-09 18:09:31 +0100

    Initialized Window Creator class

--- src/window/window_creater.java
+++ src/window/window_creater.java
@@ -15,29 +15,35 @@ public class window_creater {
     // Method to create a new Textfield
     // requires: four bound values, defining where the tf should appear.
     public static JTextField createNewTestField(int x, int y, int width, int height) {
-        final JTextField text_field=new JTextField();
+        final JTextField text_field = new JTextField();
         text_field.setBounds(x, y, width, height);
         return text_field;
     }
 
     public static JButton createNewJButton(String text, int x, int y, int width, int height) {
-        JButton button=new JButton(text);
+        JButton button = new JButton(text);
         button.setBounds(x, y, width, height);
         return button;
     }
 
-    public static void startWindow(){
+    public static void startWindow() {
+
+    }
+
+    public static ActionListener addActionListener() {
 
     }
 
 
     JFrame f = createNewWindow("Test");
-    final JTextField tf=new JTextField();
-        tf.setBounds(50,50, 150,20);
-    JButton b=new JButton("Click Here");
+    final JTextField tf = new JTextField();
+        tf.setBounds(50,50,150,20);
+    JButton b = new JButton("Click Here");
         b.setBounds(50,100,95,30);
-        b.addActionListener(new ActionListener(){
-        public void actionPerformed(ActionEvent e){
+        b.addActionListener(new
+
+    ActionListener() {
+        public void actionPerformed (ActionEvent e){
             tf.setText("Welcome to Javatpoint.");
         }
     });
